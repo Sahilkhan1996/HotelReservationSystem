04528fdd73bc2b8f73c04b1c4da3960788899517
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-01 20:27:41 +0530

    Adding Hotels

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,6 +1,24 @@
 
 public class ReservationSystem {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation Program");
+	private String name;
+	private Double rate;
+	public String getName() {
+		return name;
 	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getRate() {
+		return rate;
+	}
+	public void setRate(Double rate) {
+		this.rate = rate;
+	}
+	@Override
+	public String toString() {
+		return " [ Hotel name=" + name + ", Rate of the Hole =" + rate + "]";
+	}
+	
+	
+	
 }
